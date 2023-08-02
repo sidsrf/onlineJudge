a96e3722b08ae31c89ad1aac46aa65f4df845355
Siddharth Saraf <sidsrf@gmail.com>
2023-08-02 22:28:48 +0530

    fix 3

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
 
 const { Submission } = require("./db");
 const passport = require("./passport");
@@ -21,7 +21,7 @@ app.use(
     credentials: true,
   })
 );
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
